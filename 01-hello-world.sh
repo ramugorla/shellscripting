ff1dd084e3fa7fd289b5ca5f02d7e27f1c4a8613
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-08-05 00:00:39 +0530

    Related to CMD line args

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -4,6 +4,8 @@
 
 #First line is shebang it is the interpreter to execute the command or syntax inside the shell script
 
+# C shell or Z shell or K shell or Shell but it all are equal to bash 
+
 echo "Hello World"
 
 echo "Hi Ramu Gorla"
---
+++ 04-varaibles-args.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 :: Hi $PERSON2"
+echo "$PERSON2 :: Hlo $PERSON1"
+echo "$PERSON1 :: How are u $PERSON2"
+echo "$PERSON2 :: Yes i am Fyn $PERSON1"
