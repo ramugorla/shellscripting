31395865bd1df69e1a82d2b4e2029e66775ccbf8
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-08-05 00:19:12 +0530

    Arrays

---
+++ 07-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+MOVIES=(RRR, NTR, DEVARA)
+
+echo "First Movie is: $MOVIES[0]"
+echo "second Movie is: ${MOVIES[1]}"
+echo "All Movies are: ${MOVIES[@]}"
+echo "second Movie is: ${MOVIES[-2]}"
