3e3817b72cb789a7e04bdeb3ae0289a6cc5f7272
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-08-04 23:55:36 +0530

    Related to variables

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 
+# This line is commented 
+
+#First line is shebang it is the interpreter to execute the command or syntax inside the shell script
+
 echo "Hello World"
 
 echo "Hi Ramu Gorla"
---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+PERSON1=RAMESH
+PERSON2=SURESH
+
+#Variable Declaration in shell script is  $PERSON1   or ${PERSON1}
+
+echo "$PERSON1 :: Hi $PERSON2"
+echo "${PERSON2} :: Hlo $PERSON1"
+echo "$PERSON1 :: How are u $PERSON2"
+echo "$PERSON2 :: Yes i am Fyn $PERSON1"
