368036afd2b25dab709535cddec8708f64f94629
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-08-06 08:19:25 +0530

    package installation

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -7,13 +7,25 @@ TIMESTAMP=$(date)
 
 if [ $USERID -ne 0 ]
 then 
-    echo " ERROR: To run this command you should have root user access"
+    echo "ERROR: To run this command you should have root user access"
     exit 1
 fi 
 
 echo "Script is executed at: $TIMESTAMP"
 
 
-dnf install mysqll -y 
+dnf install mysqll -y
+if [ $? -ne 0 ]
+then
+    echo "Installing MYSQL ... FAILURE"
+else
+    echo "Installing MYSQL ... SUCCESS"
+fi 
 
-#dnf install git -y
+dnf install git -y
+if [ $? -ne 0 ]
+then
+    echo "Installing GIT ... FAILURE"
+else
+    echo "Installing GIT ... SUCCESS"
+fi
