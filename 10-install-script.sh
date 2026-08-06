1bf6be8766af23319102c5c2e79d5b4a75cb8a01
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-08-06 08:15:34 +0530

    package installation

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -8,7 +8,7 @@ TIMESTAMP=$(date)
 if [ $USERID -ne 0 ]
 then 
     echo " ERROR: To run this command you should have root user access"
-    #exit 1
+    exit 1
 fi 
 
 echo "Script is executed at: $TIMESTAMP"
