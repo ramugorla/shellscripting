caba36112356cc1b621628477d0637e3f28d96fd
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-08-06 08:01:25 +0530

    package installation

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
+
+TIMESTAMP=$(date)
+
+if [ $USERID -ne 0 ]
+then 
+    echo " ERROR: To run this command you should have root user access"
+
+fi 
+
+echo "Script is executed at: $TIMESTAMP"
+
+
 dnf install mysql -y 
