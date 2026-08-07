d2aaddbaf6e9d51871b7c65fdf23cf0111b6a9ef
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-08-07 09:57:41 +0530

    Disk usage

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -2,11 +2,18 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5
+MSG=""
 
 
 while read -r line
 do 
-    USAGE=$(echo $line | awk -F " " '{print $6F}')
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTION=$(echo $line | awk -F " " '{print $NF}')
-    echo "Partion is $PARTION and usage is $USAGE"
+    #echo "Partion is $PARTION and usage is $USAGE"
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG="High disk usage on partiotion $PARTION and disk usage is $USAGE"
+
 done <<< "$DISK_USAGE"
+
+echo $MSG
