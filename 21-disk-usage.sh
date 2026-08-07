ab1178a30acd4115dce05e75007734e02e3f9da9
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-08-07 09:52:44 +0530

    Disk usage

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -6,5 +6,7 @@ DISK_THRESHOLD=5
 
 while read -r line
 do 
-    echo $line
+    USAGE=$(echo $line | awk -F " " '{print $6F}')
+    PARTION=$(echo $line | awk -F " " '{print $NF}')
+    echo "Partion is $PARTION and usage is $USAGE"
 done <<< "$DISK_USAGE"
