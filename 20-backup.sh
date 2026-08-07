d34c6d0bc6402d90ecba685fe74920087b9b41c2
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-08-07 09:17:09 +0530

    logs moving to backup

--- 20-backup.sh
+++ 20-backup.sh
@@ -12,7 +12,11 @@ DAYS=${3:-14}
 
 SOURCE="/home/ec2-user/app-logs"
 DEST="/home/ec2-user/archieve"
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
 
 USAGE(){
     echo "USAGE:: <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
@@ -25,6 +29,7 @@ fi
 
 mkdir -p $SOURCE
 mkdir -p $DEST
+mkdir -p $LOGS_FOLDER
 
 if [ ! -d $SOURCE_DIR ]  # To check whether the dir is existing or not if not this condition will True
 then
@@ -45,7 +50,7 @@ if [ -n "$FILES" ] #true if there are files
 then
     echo "Files to zip"
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP"
-    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | Zip -@ "$ZIP_FILE"
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
     if [ ! -f "$ZIP_FILE" ]
     then
         echo "Succefully created Zip file"
