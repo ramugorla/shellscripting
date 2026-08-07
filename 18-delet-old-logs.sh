f033331dbc6b48c64822cae64fef8f6e242669b8
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-08-07 08:23:53 +0530

    Deleting old logsin server

--- 18-delet-old-logs.sh
+++ 18-delet-old-logs.sh
@@ -17,3 +17,10 @@ mkdir -p $SOURCE_DIR
 Files_to_be_deleted=$(find $SOURCE_DIR -name "*.log" -mtime +14 | awk -F "/" '{print $NF}')
 
 echo $Files_to_be_deleted
+
+
+while read -r file
+do 
+    echo $file
+    
+done <<<$Files_to_be_deleted
