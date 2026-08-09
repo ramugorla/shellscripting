eb3691eb184a568983dee8997e5d1267d4591989
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-08-09 10:19:26 +0530

    all shell script

--- 22-all-shell-script.sh
+++ 22-all-shell-script.sh
@@ -54,12 +54,23 @@
 # echo "List of all Movies are: ${MOVIES[@]}"
 
 
-echo "All variables passed: $@"
-echo "Number of passed: $#"
-echo "Script name: $0"
-echo "Present working dir: $PWD"
-echo "current script user: $USER"
-echo "current user home dir: $HOME"
-echo "PID of current script: $$"
-sleep 10 &
-echo "PID of last cmd in background: $!"
+# echo "All variables passed: $@"
+# echo "Number of passed: $#"
+# echo "Script name: $0"
+# echo "Present working dir: $PWD"
+# echo "current script user: $USER"
+# echo "current user home dir: $HOME"
+# echo "PID of current script: $$"
+# sleep 10 &
+# echo "PID of last cmd in background: $!"
+
+
+NUMBER=$1
+
+if [ $NUMBER -gt 15 ]
+then
+    echo "Given $NUMBER is greater than 15"
+else
+    echo "Given $NUMBER is less than 15"
+fi 
+
