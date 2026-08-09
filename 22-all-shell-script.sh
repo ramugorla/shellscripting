5ad0fac2d2cfca342bfa23047783a8c81734aaf3
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-08-09 10:06:41 +0530

    all shell script

--- 22-all-shell-script.sh
+++ 22-all-shell-script.sh
@@ -2,36 +2,46 @@
 
 #this line is commented
 
-echo "Hello world"
-echo "Hi Ramu"
+# echo "Hello world"
+# echo "Hi Ramu"
 
-PERSON1=RAMU
-PERSON2=LAXMAN
+# PERSON1=RAMU
+# PERSON2=LAXMAN
 
-echo "$PERSON1: Hi $PERSON2"
-echo "${PERSON2}: Hlo $PERSON1"
-echo "$PERSON1: H R U $PERSON2"
-echo "$PERSON2: Fyn $PERSON1 and wt abt U"
+# echo "$PERSON1: Hi $PERSON2"
+# echo "${PERSON2}: Hlo $PERSON1"
+# echo "$PERSON1: H R U $PERSON2"
+# echo "$PERSON2: Fyn $PERSON1 and wt abt U"
 
-PERSON1=$1
-PERSON2=$2
-
-
-echo "$PERSON1: Hi $PERSON2"
-echo "${PERSON2}: Hlo $PERSON1"
-echo "$PERSON1: H R U $PERSON2"
-echo "$PERSON2: Fyn $PERSON1 and wt abt U"
+# PERSON1=$1
+# PERSON2=$2
 
 
-echo "Please enter your user name: "
+# echo "$PERSON1: Hi $PERSON2"
+# echo "${PERSON2}: Hlo $PERSON1"
+# echo "$PERSON1: H R U $PERSON2"
+# echo "$PERSON2: Fyn $PERSON1 and wt abt U"
 
-read -s USERNAME
 
-echo "$USERNAME"
+# echo "Please enter your user name: "
 
-echo "Please enter your password: "
+# read -s USERNAME
 
-read -s PASSWORD 
+# echo "$USERNAME"
 
-echo "$PASSWORD"
+# echo "Please enter your password: "
+
+# read -s PASSWORD 
+
+# echo "$PASSWORD"
+
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(( $NUMBER1 + $NUMBER2 ))
+
+echo "Sum of $NUMBER1 and $NUMBER2 is: $SUM"
+
+echo $(date)
 
