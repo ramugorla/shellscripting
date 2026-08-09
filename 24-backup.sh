a5d92cf407a57d98a3c98fd1c71c3a89e8252d7e
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-08-09 12:44:55 +0530

    backup

--- 24-backup.sh
+++ 24-backup.sh
@@ -36,3 +36,15 @@ if [ $# -lt 2 ]
 then
     USAGE
 fi 
+
+
+if [ ! -d $SOURCE_DIR ]
+then
+    echo -e "$SOURCE_DIR Does not exist ... $Y please check $N"
+fi 
+
+if [ ! -d $DEST_DIR ]
+then
+    echo -e "$DEST_DIR Does not exist ... $Y please check $N"
+fi
+
